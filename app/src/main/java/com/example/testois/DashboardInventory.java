22ff81e3c0451320b41b3ad205689bbd4ad70aa2
Johann <johannmiguel.comia.iics@ust.edu.ph>
2022-02-15 10:48:25 +0800

    added front end for summary dashboards

---
+++ app/src/main/java/com/example/testois/DashboardInventory.java
@@ -0,0 +1,14 @@
+package com.example.testois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_inventory);
+    }
+}
